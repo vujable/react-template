1805267a1402b169e42daf422692f34f57dc5f30
Vukasin Pesic <vukasin@comit.co.rs>
2021-12-24 14:22:09 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import logo from 'assets/images/logo.png';
+import { Layout, Nav, Page, NavLogo } from '@vujable/react-layout';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -7,9 +10,18 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+      <Layout>
+        <Nav>
+          <NavLogo>
+            <img alt="logo" src={logo}></img>
+          </NavLogo>
+        </Nav>
+        <Page>
+          <h1>Down For Maintenance</h1>
+          <p>Sorry for the inconvenience, but we’re performing a maintenance at the moment.</p>
+          <p>We’ll be back online shortly!</p>
+        </Page>
+      </Layout>
     );
   }
 }
